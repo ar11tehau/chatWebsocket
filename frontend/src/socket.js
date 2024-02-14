04354ad01ad8e5a935084cc453399ca4aaf8f96c
Arii D <108074652+ar11tehau@users.noreply.github.com>
2024-02-14 11:07:12 +0100

    url socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5173/");
+const socket = io("https://chat.domelier.fr/");
 socket.emit("message", "WebSocket connection established");
 
 export default socket
